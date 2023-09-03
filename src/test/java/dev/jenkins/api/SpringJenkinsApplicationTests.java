4ee45fd0f7ad9162c0380e811d35016d4c7be9f6
simple-deve1oper <simple-deve1oper>
2023-09-03 23:16:35 +0300

    no error mow

--- src/test/java/dev/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/dev/jenkins/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ class SpringJenkinsApplicationTests {
 	private static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing ...");
 		logger.info("Test case executing second log statement ...");
 
