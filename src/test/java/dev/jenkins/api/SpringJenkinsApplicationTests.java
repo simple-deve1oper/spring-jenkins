7713048ee32223c2e9fea621355114b5d52adc15
simple-deve1oper <simple-deve1oper>
2023-09-04 00:01:49 +0300

    removed logger statement

--- src/test/java/dev/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/dev/jenkins/api/SpringJenkinsApplicationTests.java
@@ -14,7 +14,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing ...");
-		logger.info("Test case executing second log statement ...");
 
 		assertEquals(true, true);
 	}
